1385555b16644f988f814495fa1facafa4940922
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-09-22 09:04:44 +0200

    Lesson 107. Jackson Property Naming Strategies

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest {
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Test
+	void testSnake() throws JsonProcessingException {
+		BeerDto beerDto = getDto();
+
+		String jsonString = objectMapper.writeValueAsString(beerDto);
+
+		System.out.println(jsonString);
+	}
+}
